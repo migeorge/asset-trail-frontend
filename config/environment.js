abd8b0bb965d5468686a902f209009f0d350ae4f
Mike George <mike@tallduck.com>
2016-01-03 17:31:45 -0500

    base app setup

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'asset-trail-frontend',
+    podModulePrefix: 'asset-trail-frontend/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
